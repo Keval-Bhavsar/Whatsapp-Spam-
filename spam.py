1f107334d406be736db5075111a119679718a2d2
Keval_@t_Github <84740928+Keval-Bhavsar@users.noreply.github.com>
2022-07-22 00:50:11 +0530

    Add files via upload

---
+++ spam.py
@@ -0,0 +1,65 @@
+import pyautogui
+import webbrowser as wb
+import time
+
+wb.open("https://web.whatsapp.com/")
+time.sleep(25)
+for i in range(90):
+    pyautogui.press(".")
+    pyautogui.press("B")
+    pyautogui.press("A")
+    pyautogui.press("S")
+    pyautogui.press("S")
+    pyautogui.press(" ")
+    pyautogui.press(" ")
+    pyautogui.press("B")
+    pyautogui.press("A")
+    pyautogui.press("N")
+    pyautogui.press("D")
+    pyautogui.press(" ")
+    pyautogui.press("K")
+    pyautogui.press("A")
+    pyautogui.press("R")
+    pyautogui.press("O")
+    pyautogui.press(" ")
+    pyautogui.press(" ")
+    pyautogui.press(" ")
+    pyautogui.press(" ")
+    pyautogui.press(" ")
+    pyautogui.press(" ")
+    pyautogui.press(" ")     
+    pyautogui.press(" ")
+    pyautogui.press("Enter")
+      
+
+
+
+
+
+# import pyautogui
+# import webbrowser as wb
+# import time
+
+# ph = "+917778866663"
+
+# wb.open("https://wa.me/" + ph)
+# time.sleep(20)
+# for i in range(10):
+#                 pyautogui.press("h")
+#                 pyautogui.press("i")
+#                 pyautogui.press("i")
+#                 pyautogui.press("i")
+#                 pyautogui.press("i")
+#                 pyautogui.press("enter")
+#                 pyautogui.press("s")
+#                 # pyautogui.press("enter")
+#                 pyautogui.press("u")
+#                 # pyautogui.press("enter")
+#                 pyautogui.press("k")
+#                 # pyautogui.press("enter")
+#                 pyautogui.press("a")
+#                 # pyautogui.press("enter")
+#                 pyautogui.press("r")
+#                 # pyautogui.press("enter")
+#                 pyautogui.press("e")
+#                 pyautogui.press("enter")
